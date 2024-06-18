9605078cc03b8552ebc87930240f3d8f484fe1f2
Anton Krylov <antonkrylov@antons-mbp.home>
2024-06-18 23:09:22 +0100

    fix

--- youtube-summary-extension/src/contentScript/youtube.ts
+++ youtube-summary-extension/src/contentScript/youtube.ts
@@ -1,11 +1,24 @@
 import { wait } from ".";
-import { save } from "./youtube";
-import { openTranscript } from "./youtube";
 import { TRANSCRIPTION_FILE, DESCRIPTION_FILE } from "../constants";
 
 import { waitFor } from "../helpers";
 import { saveChunks } from "./storage";
 
+function save(filename, data) {
+    const blob = new Blob([data], { type: 'text/plain' });
+    if (window.navigator.msSaveOrOpenBlob) {
+      window.navigator.msSaveBlob(blob, filename);
+    }
+    else {
+      const elem = window.document.createElement('a');
+      elem.href = window.URL.createObjectURL(blob);
+      elem.download = filename;
+      document.body.appendChild(elem);
+      elem.click();
+      document.body.removeChild(elem);
+    }
+}
+  
 
 export const createTranscriptButton = async () => {
   const buttonContainer = await waitFor(10000, '#actions');
@@ -25,7 +38,9 @@ export const createTranscriptButton = async () => {
   transcriptButton.addEventListener('click', () => {
     openTranscript();
   });
-};export const openTranscript = () => {
+};
+
+export const openTranscript = () => {
   const moreButton = document.querySelector('#expand');
   if (moreButton) {
     moreButton.click();
@@ -59,18 +74,3 @@ export const createTranscriptButton = async () => {
     }, 2000);
   }
 };
-export function save(filename, data) {
-  const blob = new Blob([data], { type: 'text/plain' });
-  if (window.navigator.msSaveOrOpenBlob) {
-    window.navigator.msSaveBlob(blob, filename);
-  }
-  else {
-    const elem = window.document.createElement('a');
-    elem.href = window.URL.createObjectURL(blob);
-    elem.download = filename;
-    document.body.appendChild(elem);
-    elem.click();
-    document.body.removeChild(elem);
-  }
-}
-
