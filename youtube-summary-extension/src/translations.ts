df390f8f826c639ab89c79f0ddf1bdc2414428fc
Anton Krylov <antonkrylov@antons-mbp.home>
2024-06-18 23:38:48 +0100

    fix locales

--- youtube-summary-extension/src/translations.ts
+++ youtube-summary-extension/src/translations.ts
@@ -1,12 +1,16 @@
 import { I18n } from "i18n-js";
 
+const en = {
+    pre_description: "Using description",
+    post_description: "And the transcription of the video from the file, make a summary in Russian with highlighting the main parts in the form of a detailed article. If you are unable to provide a complete response, give the opportunity to continue working with the file."
+};
+
 export const i18n = new I18n({
-    en: {
-        pre_description: "Using description",
-        post_description: "And the transcription of the video from the file, make a summary in Russian with highlighting the main parts in the form of a detailed article. If you are unable to provide a complete response, give the opportunity to continue working with the file."
-    },
+    en,
+    'en-US': en,
+    'en-UK': en,
     ru: {
         pre_description: 'Используя описание',
         post_description: 'и транскрипцию видео из файла, сделай саммари на русском с выделением основных частей в формате подробной статьи. В случае если не сможешь дать полный ответ - дай возможность продолжить работать с файлом'
-    }
+    },
 });
