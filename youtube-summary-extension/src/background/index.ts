654a3ce6bb3d39a41fa3c9e8f70038b5451b050c
pivaszbs <toxa121211@gmail.com>
2024-06-26 23:12:02 +0100

    update translations and model

--- youtube-summary-extension/src/background/index.ts
+++ youtube-summary-extension/src/background/index.ts
@@ -2,7 +2,7 @@ import { SECRET_KEYWORD } from "../constants";
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'openChatGPT') {
-    chrome.tabs.create({ url: `https://chatgpt.com/?from=${SECRET_KEYWORD}` });
+    chrome.tabs.create({ url: `https://chatgpt.com/?from=${SECRET_KEYWORD}&model=gpt-4o` });
   }
 });
 
--- youtube-summary-extension/src/translations.ts
+++ youtube-summary-extension/src/translations.ts
@@ -2,7 +2,7 @@ import { I18n } from "i18n-js";
 
 const en = {
     pre_description: "Using description",
-    post_description: "And the transcription of the video from the file, make a summary in English with highlighting the main parts in the form of a detailed article. If you are unable to provide a complete response, give the opportunity to continue working with the file."
+    post_description: "And transcribe the video from the file, summarize it in Russian highlighting the main parts in the form of a detailed article. If you are unable to provide a complete response, give the opportunity to continue working with the file. Please do not include timestamps in the response."
 };
 
 const locales = {
@@ -11,7 +11,7 @@ const locales = {
     'en-GB': en,
     ru: {
         pre_description: 'Используя описание',
-        post_description: 'и транскрипцию видео из файла, сделай саммари на русском с выделением основных частей в формате подробной статьи. В случае если не сможешь дать полный ответ - дай возможность продолжить работать с файлом'
+        post_description: 'и транскрипцию видео из файла, сделай саммари на русском с выделением основных частей в формате подробной статьи. В случае если не сможешь дать полный ответ - дай возможность продолжить работать с файлом. Пожалуйста не включай таймкоды в ответ'
     },
 };
 
